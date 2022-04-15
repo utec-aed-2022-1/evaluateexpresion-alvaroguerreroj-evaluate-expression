ffa4347f992de284f80d9eadbf918e53cf18608d
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-04-14 19:46:36 -0500

    refactor(treewide): `clang-format`

--- include/solution.hpp
+++ include/solution.hpp
@@ -3,18 +3,18 @@
 #include <iostream>
 #include <string>
 
-struct Result{
+struct Result
+{
     double result;
     bool error;
-}
-
+};
 
 Result evaluate(std::string input)
 {
     // 1- descomponer el input y validar
 
     // 2- convertir de Infijo a Postfijo
-    
+
     // 3- resolver la expresion
 
     //* Si no cumple la validacion retornar Result.error = true;
