91e555b7875ffd7c89f6858ee337c04a9106241d
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-04-14 19:27:22 -0500

    build(.dir-locals): Add

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((git-commit-mode . ((git-commit-summary-max-length . 72)))
+ (c++-mode . ((fill-column . 96))))
