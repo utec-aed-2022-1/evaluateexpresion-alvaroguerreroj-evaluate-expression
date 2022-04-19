de73ffac86a48e6dcd9b3ab386484d190b517a97
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-04-19 18:41:38 -0500

    build(dir-locals): Don't format with lsp

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,8 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((git-commit-mode . ((git-commit-summary-max-length . 72)))
- (c++-mode . ((fill-column . 96))))
+ (c++-mode . ((fill-column . 96)
+              ;; clang-tools (including clangd) is version 13 and we are using
+              ;; version 14 configuration options, this makes it so it calls
+              ;; clang-format which is provided by clang_14
+              (+format-with-lsp . nil))))
