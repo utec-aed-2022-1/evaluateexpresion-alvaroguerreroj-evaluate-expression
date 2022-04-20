b7988e1fceb2987a569d1daccf021b77467068d3
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2022-04-19 19:21:49 -0500

    feat(include/solution): Add

--- include/solution.hpp
+++ include/solution.hpp
@@ -1,23 +1,45 @@
 #pragma once
 
-#include <iostream>
+#include <ostream>
+#include <sstream>
+#include <stdexcept>
 #include <string>
+#include <variant>
+#include <vector>
+
+using symbol = std::variant<double, char>;
 
 struct Result
 {
     double result;
     bool error;
+
+    auto operator==(Result const& other) const -> bool
+    {
+        return result == other.result && error == other.error;
+    }
+
+    friend auto operator<<(std::ostream& out, Result const& result) -> std::ostream&
+    {
+        out << "<" << result.result << ", " << result.error << ">";
+
+        return out;
+    }
 };
 
-auto evaluate(std::string input) -> Result
+class InfixError : public std::exception
 {
-    // 1- descomponer el input y validar
+public:
+    [[nodiscard]] auto what() const noexcept -> char const* override
+    {
+        return "InfixError";
+    }
+};
 
-    // 2- convertir de Infijo a Postfijo
+auto evaluate(std::string const& input) -> Result;
+auto infix_to_postfix(std::vector<symbol> const& vs) -> std::vector<symbol>;
+auto tokenize(std::string const& input) -> std::vector<symbol>;
 
-    // 3- resolver la expresion
-
-    //* Si no cumple la validacion retornar Result.error = true;
-
-    return Result();
-}
+auto operator<<(std::ostream& out, std::variant<double, char> const& v) -> std::ostream&;
+auto operator<<(std::ostream& out, std::vector<std::variant<double, char>> const& v)
+    -> std::ostream&;
